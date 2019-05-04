ac47e30af87ad7304a5d378b8c9e58f84eee2818
Aasim Khan <Aasim_Khan@external.mckinsey.com>
2019-05-04 13:01:44 +0530

    Added structure

---
+++ src/parking_lot.py
@@ -0,0 +1,12 @@
+
+
+class ParkingLot:
+    def __init__(self, total_slots):
+        self.total_slots = total_slots
+        self.available_slots = None
+
+    def create_parking_lot(self):
+        pass
+
+    def assign_slot(self):
+        pass
---
+++ src/utility.py
@@ -0,0 +1,6 @@
+def load_file(filename):
+    pass
+
+
+def write_to_file(filename, data):
+    pass
